ea092f8be15d668fe4d4fe05892cbce03b8bae28
harjot-oberai <harjot.oberai@gmail.com>
2017-05-29 02:02:55 +0530

    removes unused imports

--- materialshadows/src/main/java/com/sdsmdg/harjot/materialshadows/MaterialShadowViewWrapper.java
+++ materialshadows/src/main/java/com/sdsmdg/harjot/materialshadows/MaterialShadowViewWrapper.java
@@ -6,7 +6,6 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Path;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
 
@@ -58,10 +57,8 @@ public class MaterialShadowViewWrapper extends RelativeLayout {
                 shadowAlpha = a.getFloat(attr, 0.99f);
             } else if (attr == R.styleable.MaterialShadowViewWrapper_shadowOffsetX) {
                 offsetX = a.getFloat(attr, 0.0f);
-                Log.d("ATTR", offsetX + ";");
             } else if (attr == R.styleable.MaterialShadowViewWrapper_shadowOffsetY) {
                 offsetY = a.getFloat(attr, 0.0f);
-                Log.d("ATTR", offsetY + ";");
             }
         }
         a.recycle();
