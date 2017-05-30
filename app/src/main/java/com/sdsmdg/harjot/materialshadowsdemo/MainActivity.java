3c1d013e5f62d63d4205cd5c8da0128f27e78696
yarolegovich <yarolegovich@gmail.com>
2017-05-30 21:25:10 +0300

    changed sample so that people can see benefits of async rendering

--- app/src/main/java/com/sdsmdg/harjot/materialshadowsdemo/MainActivity.java
+++ app/src/main/java/com/sdsmdg/harjot/materialshadowsdemo/MainActivity.java
@@ -2,12 +2,35 @@ package com.sdsmdg.harjot.materialshadowsdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
-public class MainActivity extends AppCompatActivity {
+import com.sdsmdg.harjot.materialshadows.MaterialShadowViewWrapper;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private CheckBox calculateAsync;
+    private CheckBox showWhenAllReady;
+
+    private MaterialShadowViewWrapper shadowViewWrapper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        shadowViewWrapper = (MaterialShadowViewWrapper) findViewById(R.id.shadow_wrapper);
+        calculateAsync = (CheckBox) findViewById(R.id.cb_calculate_async);
+        showWhenAllReady = (CheckBox) findViewById(R.id.cb_show_when_all_ready);
+
+        findViewById(R.id.btn_re_render).setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        shadowViewWrapper.setShouldCalculateAsync(calculateAsync.isChecked());
+        shadowViewWrapper.setShowShadowsWhenAllReady(showWhenAllReady.isChecked());
+        shadowViewWrapper.requestLayout();
+    }
+
 }
